b85f8fdcb4d6669481d37cec3416f9b3834615c8
汲蛤扑 <song_csu@outlook.com>
2019-07-10 23:36:31 +0800

    Create httpclient.cpp

---
+++ HTTP/Examples/HttpClient/httpclient.cpp
@@ -0,0 +1,93 @@
+#include "httpclient.h"
+#include "curl/curl.h"
+#include <string>
+ 
+CHttpClient::CHttpClient(void) : 
+m_bDebug(false)
+{
+ 
+}
+ 
+CHttpClient::~CHttpClient(void)
+{
+ 
+}
+ 
+static int OnDebug(CURL *, curl_infotype itype, char * pData, size_t size, void *)
+{
+	if(itype == CURLINFO_TEXT)
+	{
+		//printf("[TEXT]%s\n", pData);
+	}
+	else if(itype == CURLINFO_HEADER_IN)
+	{
+		printf("[HEADER_IN]%s\n", pData);
+	}
+	else if(itype == CURLINFO_HEADER_OUT)
+	{
+		printf("[HEADER_OUT]%s\n", pData);
+	}
+	else if(itype == CURLINFO_DATA_IN)
+	{
+		printf("[DATA_IN]%s\n", pData);
+	}
+	else if(itype == CURLINFO_DATA_OUT)
+	{
+		printf("[DATA_OUT]%s\n", pData);
+	}
+	return 0;
+}
+ 
+static size_t OnWriteData(void* buffer, size_t size, size_t nmemb, void* lpVoid)
+{
+	std::string* str = dynamic_cast<std::string*>((std::string *)lpVoid);
+	if( NULL == str || NULL == buffer )
+	{
+		return -1;
+	}
+ 
+    char* pData = (char*)buffer;
+    str->append(pData, size * nmemb);
+	return nmemb;
+}
+ 
+int CHttpClient::Post(const std::string & strUrl, const std::string & strPost, std::string & strResponse)
+{
+	CURLcode res;
+	CURL* curl = curl_easy_init();
+	if(NULL == curl)
+	{
+		return CURLE_FAILED_INIT;
+	}
+	if(m_bDebug)
+	{
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+		curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, OnDebug);
+	}
+	curl_easy_setopt(curl, CURLOPT_URL, strUrl.c_str());
+	curl_easy_setopt(curl, CURLOPT_POST, 1);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, strPost.c_str());
+	curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, OnWriteData);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&strResponse);
+	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+	curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 3);
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 3);
+	res = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
+	return res;
+}
+ 
+int CHttpClient::Get(const std::string & strUrl, std::string & strResponse)
+{
+	CURLcode res;
+	CURL* curl = curl_easy_init();
+	if(NULL == curl)
+	{
+		return CURLE_FAILED_INIT;
+	}
+	if(m_bDebug)
+	{
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+		curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, OnDebug);
+	}
